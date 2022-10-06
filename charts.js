fba8882453bc7a7e3b2f8e12c25a7dd6ec6f5f46
Tomfin10 <61998604+Tomfin10@users.noreply.github.com>
2022-10-06 14:58:07 +1100

    homeworkW10
    first draft

---
+++ charts.js
@@ -0,0 +1,10 @@
+var map_vg_1 = "victims_map.json";
+vegaEmbed("#map", map_vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var death_time_vg = "death_time2.json";
+vegaEmbed("#death_time", death_time_vg).then(function(result) {
+
+}).catch(console.error);
+
